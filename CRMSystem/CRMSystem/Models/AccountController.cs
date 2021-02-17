a87e738e45b5e8d49d93f3c03e3bfd9f0fcfd110
Dinesh waghade <dineshwaghade12@gmail.com>
2021-02-17 15:28:26 +0530

    add admin template

---
+++ CRMSystem/CRMSystem/Models/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRMSystem.Models;
+
+namespace CRMSystem.Models
+{
+    public class AccountController : Controller
+    {
+        CRM_System1Entities db = new CRM_System1Entities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Regitstration()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Regitstration()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ CRMSystem/CRMSystem/Models/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRMSystem.Models;
+using System.Data.Entity;
+
+namespace CRMSystem.Models
+{
+    public class HomeController : Controller
+    {
+        CRM_System1Entities db = new CRM_System1Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var techlist = db.tblTechnologies.ToList();
+            return View(techlist);
+        }
+        [HttpGet]
+        public ActionResult AddTechnology()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddTechnology(tblTechnology tech)
+        {
+            db.tblTechnologies.Add(tech);
+            db.SaveChanges();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditTechnology(int id)
+        {
+            var tech = db.tblTechnologies.Find(id);
+            return View(tech);
+        }
+        [HttpPost]
+        public ActionResult EditTechnology(tblTechnology tech)
+        {
+            db.Entry(tech).State = EntityState.Modified;
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
